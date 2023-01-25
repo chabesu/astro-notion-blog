f17c245038dfbf253bcda80d79319a68627eaa33
chabesu <chabesublog@gmail.com>
2023-01-26 00:36:35 +0900

    Apply profileImage CSS

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -3,6 +3,6 @@ export const DATABASE_ID = import.meta.env.DATABASE_ID;
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID;
 export const NUMBER_OF_POSTS_PER_PAGE = 10;
 
-export const PUBLIC_SITE_TITLE = "（仮）誰も得しないブログ";
+export const PUBLIC_SITE_TITLE = "チャーなる（仮）";
 export const PUBLIC_SITE_DESCRIPTION =
   "astro-notion-blog でできているブログです";
